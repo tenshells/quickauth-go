24f693e36c3734541073ff510aea219037f171c5
sheltonc <shelton.dany@gmail.com>
2025-03-20 11:24:52 +0000

    take userId from env

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 		ServiceAccountPath: os.Getenv("QS_GO_SA_JSON"),
 		APIKey:            os.Getenv("QS_GO_API_KEY"),
 	}
-	
+
 	if config.ServiceAccountPath == "" {
 		log.Print("Please add env variable for QS_GO_SA_JSON as path to service account.json")
 	}
@@ -27,10 +27,19 @@ func main() {
 		log.Fatalf("Failed to create client: %v", err)
 	}
 
-	token, err := client.GenerateIDToken(context.Background(), "user@example.com")
+
+	userId := os.Getenv("QGID")
+	if userId == "" {
+		log.Print("empty user id recieved, overriding with sheltons dev uid ")
+	}
+	userId = "R4s7qc4JYgOYXAEX5B1Vua6mAko1"
+
+	token, err := client.GenerateIDToken(context.Background(), userId)
 	if err != nil {
 		log.Fatalf("Failed to generate token: %v", err)
 	}
 
-	fmt.Printf("Generated ID token: %s\n", token)
+	fmt.Printf("Generated ID token for user: %s, on project: %s\n", userId, config.ServiceAccountPath)
+	fmt.Printf("\n%s\n", token)
+
 }
